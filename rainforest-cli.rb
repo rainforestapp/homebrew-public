753a76af6ce32368000d79deb42fd4b655c41faa
Russell Smith <ukd1@users.noreply.github.com>
2018-05-15 16:19:10 -0600

    Use a fixed version, so this doesn't break

--- rainforest-cli.rb
+++ rainforest-cli.rb
@@ -1,7 +1,7 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://bin.equinox.io/c/htRtQZagtfg/rainforest-cli-stable-darwin-amd64.tgz"
+  url "https://bin.equinox.io/a/cieAcRG8XNr/rainforest-cli-2.8.12-darwin-amd64.tar.gz"
   sha256 "2cfcce83cdd40ed8bbc3e5fce6f30c8e515348022a7ab1d84266b0fc1a7cc885"
 
   def install
