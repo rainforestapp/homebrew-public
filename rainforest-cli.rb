41803fc53de534109e009dacbd70a4c1ab47d0d6
Russell Smith <ukd1@users.noreply.github.com>
2019-07-23 17:17:51 -0700

    Update to the latest 2.14.0 release

--- rainforest-cli.rb
+++ rainforest-cli.rb
@@ -1,8 +1,8 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://bin.equinox.io/a/87Am2QMqUHm/rainforest-cli-2.13.0-darwin-amd64.tar.gz"
-  sha256 "d16db33f34d82d41a87c45f3eb7dcc9e6a740515a9620c2b01bf0ca6757e140e"
+  url "https://bin.equinox.io/a/niBq1ctCV7F/rainforest-cli-2.14.0-darwin-amd64.tar.gz"
+  sha256 "51a474759a8aca1ca6b87265b8062785299ce459cf071cf934a8b2901abafd7a"
 
   def install
     bin.install "rainforest"
