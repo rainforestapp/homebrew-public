eef05b895bc940c583afe07ee5623924fc5bfb0f
Paul Padier <paul.padier@rainforestapp.com>
2020-07-08 21:52:49 +0900

    Update rainforest-cli to 2.16.0
    https://github.com/rainforestapp/rainforest-cli/releases/tag/v2.16.0

--- rainforest-cli.rb
+++ rainforest-cli.rb
@@ -1,8 +1,8 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://bin.equinox.io/a/76a467FDqyq/rainforest-cli-2.15.3-darwin-amd64.tar.gz"
-  sha256 "17f86fd780a86fe77b75d7a3f4271407ff0547d61f2058e898e011a2d67c9172"
+  url "https://bin.equinox.io/a/2HryZFko31r/rainforest-cli-2.16.0-darwin-amd64.tar.gz"
+  sha256 "b36e5022ce9664a355b60fdd0e308a4f8bbeb338ff505174cd365c3028cd01d9"
 
   def install
     bin.install "rainforest"
