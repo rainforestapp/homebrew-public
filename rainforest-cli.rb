248d24ca8659c3c21cb1cba33457ee9e2b0551eb
Maciej Gryka <maciej@rainforestapp.com>
2021-01-15 16:14:58 +0100

    release rainforest-cli 2.18.0

--- rainforest-cli.rb
+++ rainforest-cli.rb
@@ -1,8 +1,8 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://bin.equinox.io/a/4ANpxdTeURo/rainforest-cli-2.17.0-darwin-amd64.tar.gz"
-  sha256 "968dead9f25de0f9d0df0d8160b90773117c644eacad3679c5144362aee8cba5"
+  url "https://bin.equinox.io/a/6DV2vomRCRd/rainforest-cli-2.18.0-darwin-amd64.tar.gz"
+  sha256 "2264cd9050442710727e67546aaa5e771c3f996e2c89a22442afda37d209d13a"
 
   def install
     bin.install "rainforest"
