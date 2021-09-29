57248df9d6e8313c2be5bceb22c0631942b9ab5f
Russell Smith <ukd1@users.noreply.github.com>
2021-09-29 14:00:34 -0700

    Update to 2.22.3, add some basic tests

--- Formula/rainforest-cli.rb
+++ Formula/rainforest-cli.rb
@@ -1,8 +1,8 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://github.com/rainforestapp/rainforest-cli/releases/download/v2.22.2/rainforest-cli-2.22.2-darwin-amd64.tar.gz"
-  sha256 "0b6d7149c845d31bc4bf10ff4487bace6e2fd88371084f002efe5dd97efea3a9"
+  url "https://github.com/rainforestapp/rainforest-cli/releases/download/v2.22.3/rainforest-cli-2.22.3-darwin-amd64.tar.gz"
+  sha256 "18fb896a89d9751d2d22ecab043b6e8f104a4287fb0ef7d911319a577d9ae0d0"
 
   def install
     bin.install "rainforest"
