825c0a193b4cd2e4dc5035dafc21a3b5544b2868
Nemanja Keča <nemanja.keca@gmail.com>
2022-02-25 14:32:46 +0100

    Fix SHA

--- Formula/rainforest-cli.rb
+++ Formula/rainforest-cli.rb
@@ -2,7 +2,7 @@ class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
   url "https://github.com/rainforestapp/rainforest-cli/releases/download/v2.28.0/rainforest-cli-2.28.0-darwin-amd64.tar.gz"
-  sha256 "bc57953850f7a0e9af56b2d40e7842739d97818eea70e7fdd5237669a4c85"
+  sha256 "bc57953850f7a0e9af56b2d40e7842739d97818eea70e7fdd5237669a4c8548c"
 
   def install
     bin.install "rainforest"
