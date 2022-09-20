ca0a35f86302613ec4faced755c7657f4858e535
Paul Padier <paul.padier@outlook.com>
2022-09-20 10:41:20 +0900

    Update CLI to 3.0.0

--- Formula/rainforest-cli.rb
+++ Formula/rainforest-cli.rb
@@ -1,8 +1,8 @@
 class RainforestCli < Formula
   desc "Rainforest QA command line interface"
   homepage "https://github.com/rainforestapp/rainforest-cli"
-  url "https://github.com/rainforestapp/rainforest-cli/releases/download/v2.29.0/rainforest-cli-2.29.0-darwin-amd64.tar.gz"
-  sha256 "97dae756d27ef032f1899d7f9766858bf19d28d8336abac2060f6459e8b02793"
+  url "https://github.com/rainforestapp/rainforest-cli/releases/download/v3.0.0/rainforest-cli-3.0.0-darwin-amd64.tar.gz"
+  sha256 "28f124f204d051809dccc64929f0605335b6aca798265f93324e3cdacc1460bd"
 
   def install
     bin.install "rainforest"
